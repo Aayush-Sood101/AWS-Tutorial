ba7f297c66253059bbdc5e1d571f7668e05cde59
Aayush Sood <aayushsoodhp@gmail.com>
2025-06-07 18:13:16 +0530

    s3-done

---
+++ s3-tutorial/index.js
@@ -0,0 +1,28 @@
+const {S3Client , GetObjectCommand} = require("@aws-sdk/client-s3")
+const {getSignedUrl} = require("@aws-sdk/s3-request-presigner")
+require('dotenv').config();
+
+const s3Client = new S3Client({
+        region: 'ap-south-1',
+        credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        }
+})
+
+async function getObjectURL(key){
+        const command = new GetObjectCommand({
+                Bucket: 'bucket.aayushs3.private',
+                Key: key
+        })
+
+        const url = await getSignedUrl(s3Client , command)
+        return url;
+}
+
+async function init(){
+        const url = await getObjectURL("images/CISCE Results 2021.pdf");
+        console.log("URL: ", url);
+}
+
+init();
